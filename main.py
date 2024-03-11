17b36620fd40d79c8a3668679cd7abdef1b62662
Arihant Jindal <arihant23119@iiitd.ac.in>
2024-03-11 12:36:56 +0530

    Updated

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 register_dict = {
    "zero": "00000",
    "ra": "00001",
@@ -11,28 +9,29 @@ register_dict = {
     "t2": "00111",
     "s0": "01000",
     "s1": "01001",
-   "a0": "01010",
-   "a1": "01011",
-   "a2": "01100",  
-   "a3": "01101",
-   "a4": "01110",
-   "a5": "01111",
-   "a6": "10000",
-   "a7": "10001",
-   "s2": "10010",  
-   "s3": "10011",
-   "s4": "10100",
-   "s5": "10101",
-   "s6": "10110",
-   "s7": "10111",
-   "s8": "11000",
-   "s9": "11001",
-   "s10": "11010",
-   "s11": "11011",
-   "t3": "11100",  
-   "t4": "11101",
-   "t5": "11110",
-   "t6": "11111",
+
+    "a0": "01010",
+    "a1": "01011",
+    "a2": "01100",  
+    "a3": "01101",
+    "a4": "01110",
+    "a5": "01111",
+    "a6": "10000",
+    "a7": "10001",
+    "s2": "10010",  
+    "s3": "10011",
+    "s4": "10100",
+    "s5": "10101",
+    "s6": "10110",
+    "s7": "10111",
+    "s8": "11000",
+    "s9": "11001",
+    "s10": "11010",
+    "s11": "11011",
+    "t3": "11100",  
+    "t4": "11101",
+    "t5": "11110",
+    "t6": "11111",
 }
 
 r_type = {'add':"000", 'sub':"000", 'sll':"001",'slt':"010", 'sltu':"011", 'xor':"100", 'srl':"101", 'or':"110", 'and':"111"}
@@ -40,9 +39,9 @@ r_type = {'add':"000", 'sub':"000", 'sll':"001",'slt':"010", 'sltu':"011", 'xor'
 #Opcode is 0110011
 
 i_type = {"lw": ["0000011","010"], 
-          "addi": ["010011","000"], 
-          "sltiu": ["010011","011"], 
-          "jalr":["110011","000"]}
+          "addi": ["0010011","000"], 
+          "sltiu": ["0010011","011"], 
+          "jalr":["1100111","000"]}
 
 s_type = {"sw": ["0100011", "010"]}
 
@@ -54,9 +53,6 @@ u_type = {"lui":"0110111", "auipc":"0010111"}
 
 j_type = {"jal": "1101111"}
 #no funct3
-
-
-
 
 def tows_complement(binary):
     ones_complement = ''
@@ -99,37 +95,31 @@ def imm_to_bin(a, no_of_bits):
 
 f = open("text.txt","r")
 read = f.readlines()
-count = 1
-labels=[]
-for i in read:
-  s = ""
-  j = 0
-  while j < len(i):
-      if i[j] == ":":
-          labels.append(s)
-          s = ""
-          break
-      s += i[j]
-      j += 1
+labels = {}
+count = 0
+for j in read:
+    if ":" in j:
+        index = j.index(":")
+        labels[j[:index]]= count
+        read[count] = j[index+1:]
+    count += 1
 
 
+count = 0
 for i in read:
 
     words = i.split()
     i_list = []
     for word in words:
         i_list.extend(word.split(","))
-    if(len(i_list) != 0):   #check whether the line is not empty
+    if(len(i_list) != 0):     #just to check if there is an empty line
         if '(' in i_list[-1]:
             inst = i_list[-1].split("(")
             i_list[2] = inst[1][0:-1]
-            i_list[3] = inst[0]
-    else:   #if line is empty then continue
+            i_list.append(inst[0])
+    else:       #if line is empty then continue
         continue
-    if ":" in i:
-        print("There is a label here.")
-        continue
-
+    
     #R Type instructions
     if i_list[0] in r_type:
         if i_list == "sub":
@@ -143,10 +133,21 @@ for i in read:
     
     #I Type
     elif i_list[0] in i_type:
-        binary = imm_to_bin(int(i_list[2]),12)
-        binary+=register_dict[i_list[3]] + i_type[i_list[0]][1] + register_dict[i_list[1]] + i_type[i_list[0]][0]
-    
-       #S Type
+        
+        given_value=int(i_list[3])
+        if(given_value<-2**11 or given_value> 2**11-1):
+            print("ERROR:the immediate value is out of bounds")
+            break
+        
+        if((i_list[2] or i_list[1])not in register_dict):
+            print("ERROR: registers not defined")
+            break
+        
+        binary = imm_to_bin(int(i_list[3]),12)
+        binary= binary + register_dict[i_list[2]] + i_type[i_list[0]][1] + register_dict[i_list[1]] + i_type[i_list[0]][0]
+
+
+    #S Type
     elif i_list[0] in s_type:
         if(int(i_list[3])<-2**11 or int(i_list[3])> 2**11-1):
             print("ERROR:the immediate value is out of bounds")
@@ -155,12 +156,25 @@ for i in read:
 
         binary1 = imm_to_bin(int(i_list[3]),12)
         binary = binary1[0:7]+register_dict[i_list[1]]+register_dict[i_list[2]]+s_type[i_list[0]][1]+binary1[7:12]+ "0100011"
-    
+
     #B Type
     elif i_list[0] in b_type:
-        binary=imm_to_bin(int(i_list[3]),13)
-        binary+=binary[12]+binary[5:11]+register_dict[i_list[1]]+register_dict[i_list[2]]+binary[1:5]+binary[11]+b_type[i_list[0]]
 
+        lab = -1
+        if(i_list[3] in labels):
+            lab =(count - labels[i_list[3]])*4
+        else:
+            print(f"ERROR on line {count+1}: No such Label Found {i_list[3]}")
+            break
+
+        if(lab<-2**12 or lab> 2**12-1):
+            print(f"ERROR on line {count+1}:the immediate value is out of bounds")
+            break
+        
+        binary1=imm_to_bin(lab,13)
+        binary=binary1[12]+binary1[5:11]+register_dict[i_list[1]]+register_dict[i_list[2]]+binary1[1:5]+binary1[11]+b_type[i_list[0]]
+        
+    
     #U TYPE
     elif i_list[0] in u_type:
         given_value=int(i_list[2])
@@ -170,7 +184,7 @@ for i in read:
         
         imm=imm_to_bin(int(i_list[2]),32)
         binary=imm[1:21]+register_dict[i_list[1]]+u_type[i_list[0]]
-
+        
     # J TYPE
     elif i_list[0] in j_type:
         given_value=int(i_list[2])
@@ -181,12 +195,14 @@ for i in read:
 
         imm=imm_to_bin(int(i_list[2]),21)
         binary=imm[1]+imm[10:20]+imm[10]+imm[2:10]+register_dict[i_list[1]]+"1101111"
+    
+    count += 1
 
-    else:
-        print("ERROR: Invalid Instruction")
+
     if (i_list == []):
         continue
+    
     print(binary)
-    binary = ""
+    
     
 f.close()
